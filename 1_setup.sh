bc070a6dcc6766ddab11c74dfd05d3f702af1537
eazyH0 <lsh930579@gmail.com>
2025-01-26 06:58:27 +0000

    finish setup.sh maybe...

--- 1_setup.sh
+++ 1_setup.sh
@@ -1,2 +1,9 @@
-wget https://raw.githubusercontent.com/eazyH0/BAK/refs/heads/main/1_setup.sh
-chmod +x ./1_setup.sh
+wget https://raw.githubusercontent.com/eazyH0/BAK/refs/heads/main/locale -P $HOME/
+wget https://raw.githubusercontent.com/eazyH0/BAK/refs/heads/main/sources.list -P $HOME/
+chmod +x $HOME/sources.list
+chmod +x $HOME/locale
+cp -f $HOME/sources.list /etc/apt/sources.list
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install language-pack-ko tree -y
+cp -f $HOME/locale /etc/default/locale
+timedatectl set-timezone Asia/Seoul
